5bec93536baf2cca895733f744be6a6d5f7c6649
lowtix <139405785+Amine0x0@users.noreply.github.com>
2025-12-01 03:33:55 +0100

    Validate HOME environment variable in TaskManager
    Check if HOME environment variable is set before constructing taskFile path.

--- srcs/impl/Visuals.cpp
+++ srcs/impl/Visuals.cpp
@@ -17,7 +17,13 @@
 
 TaskManager::TaskManager() {
     using namespace std;
-    taskFile = string(getenv("HOME")) + "/.local/share/tvsk/.tasks";
+    taskFile = string(getenv("HOME"));
+    if(taskFile  == nullptr) {
+        std::cout <<CLR_RED  << "HOME not found " <<  CLR_RESET << std::endl;
+        exit(-1);
+    }
+    
+    taskFile = taskFile + "/.local/share/tvsk/.tasks"
     _loadTasks();
 }
 
